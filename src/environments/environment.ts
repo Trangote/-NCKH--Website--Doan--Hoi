fcc3a4fcefb0ea19779aa24d078a0c5ae5df294e
Trang <trangntt19406@st.uel.edu.vn>
2022-08-23 10:07:41 +0700

    Change rule by Trang

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseApp: {
-    apiKey: "AIzaSyB3Ar0SqZfkTt0CHzwM8DVy7Ibg1bTnwIE",
-    authDomain: "website-doan-hoi.firebaseapp.com",
-    databaseURL: "https://website-doan-hoi-default-rtdb.firebaseio.com",
-    projectId: "website-doan-hoi",
-    storageBucket: "website-doan-hoi.appspot.com",
-    messagingSenderId: "487745393201",
-    appId: "1:487745393201:web:3c6d8e7042bdb864ab3b10",
-    measurementId: "G-J9P4CPGL04"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
